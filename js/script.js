3cee5af4070fbb80f0b2971a118fe4618d32bb63
felipe97 <felipe.turen@gmail.com>
2023-11-14 09:18:13 -0800

    stated the board and winner variable, started the init function and made my board array

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 
 
 /*----- state variables -----*/
-
+let board;
+let winner;
 
 /*----- cached elements  -----*/
 
@@ -12,5 +13,15 @@
 
 /*----- functions -----*/
 
+Init();
+
+function init() {
+
+    board = [
+        [0, 0, 0, 0, 0], //col 0
+        [0, 0, 0, 0, 0], //col 1
+    ];
+console.log(board)
+}
 
 
